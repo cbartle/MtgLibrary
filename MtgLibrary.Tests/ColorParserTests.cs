9f5363f6f08c451b29a815261af8c0555f46adf6
Colln Bartley <boloman94@gmail.com>
2025-07-25 19:45:51 +0000

    Adding tests for the ParseColorsFromEffect method

--- MtgLibrary.Tests/ColorParserTests.cs
+++ MtgLibrary.Tests/ColorParserTests.cs
@@ -32,10 +32,23 @@ namespace Spiff.MtgLibrary.Tests
             List<string> colors = ColorParser.ParseColorsFromCost(input);
             Assert.That(colors, Is.EquivalentTo(expected), "The colors are not what we expected");
         }
-
-        [Test(Description="")]
-        public void InvalidCost()
+        
+        public static IEnumerable<TestCaseData> EffectText()
         {
+            yield return new TestCaseData("Return target creature you own to your hand. Flashback {W}", new List<string>() {"White"});
+            yield return new TestCaseData("Forestwalk  If one or more tokens would be created under your control, those tokens plus that many 1/1 green Squirrel creature tokens are created instead. {B}, Sacrifice X Squirrels: Target creature gets +X/-X until end of turn.", new List<string>() {"Black"});
+            yield return new TestCaseData("{T},{R}{W}{U}", new List<string>() {"White", "Red", "Blue"});
+            yield return new TestCaseData("{1}: Permanents your opponents control lose hexproof, indestructable, protection,shroud, and ward until end of turn.", new List<string>() {});
+            yield return new TestCaseData("", new List<string>() {});
+            yield return new TestCaseData(null, new List<string>() {});
+        }
+
+        [Test(Description="Tests the Effect test parsing for colors")]
+        [TestCaseSource(nameof(EffectText))]
+        public void InvalidCost(string input, IEnumerable<string> expected)
+        {
+            IEnumerable<string> colors = ColorParser.ParseColorsFromEffect(input);
+            Assert.That(colors, Is.EquivalentTo(expected), "The colors that came from the parser didnt match with what was expected");
         }
     }
 }
