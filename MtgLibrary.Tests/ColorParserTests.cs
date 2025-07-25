b2bba243d5e1d2415b030fe8d265f09d4f03d3d9
Colln Bartley <boloman94@gmail.com>
2025-07-25 14:26:05 +0000

    Adding tests for valid cost colors

---
+++ MtgLibrary.Tests/ColorParserTests.cs
@@ -0,0 +1,35 @@
+using NUnit;
+using NUnit.Framework;
+using Spiff.MtgTracker.Helpers;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Spiff.MtgLibrary.Tests
+{
+    [TestFixture]
+    public class Tests
+    {   
+        public static IEnumerable<TestCaseData> ValidCosts ()
+        {   
+            yield return new TestCaseData("2G", new List<string>() {"Green"});
+            yield return new TestCaseData("4", new List<string>(){});
+            yield return new TestCaseData("1WU", new List<string>() {"White", "Blue"});
+            yield return new TestCaseData("WBR", new List<string>() {"White","Black", "Red"});
+            yield return new TestCaseData(null, new List<string>(){});
+            yield return new TestCaseData("", new List<string>(){});
+        }
+            
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test(Description="Tests a valid cost to ensure we get the correct colors")]
+        [TestCaseSource(nameof(ValidCosts))]
+        public void ColorParser_ValidCost(string input, IEnumerable<string> expected)
+        {
+            List<string> colors = ColorParser.ParseColorsFromCost(input);
+            Assert.That(colors, Is.EquivalentTo(expected), "The colors are not what we expected");
+        }
+    }
+}
